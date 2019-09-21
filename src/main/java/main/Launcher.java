74a268ec0770480b650a124cc199fe3f4b246b63
jpconver <jpconver@gmail.com>
2019-09-21 16:26:45 -0300

    increse screen size

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -3,7 +3,7 @@ package main;
 public class Launcher {
 
     public static void main(String[] args) {
-        Game game = new Game("Convers World", 640, 480);
+        Game game = new Game("Convers World", 1024, 768);
         game.start();
     }
 
