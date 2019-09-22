7b02ca13629c0cd38c9f1b89b8d80feabe3e7fed
jpconver <jpconver@gmail.com>
2019-09-22 01:27:00 -0300

    enable opengl acceleration

--- src/main/java/application/Launcher.java
+++ src/main/java/application/Launcher.java
@@ -6,9 +6,12 @@ public class Launcher {
      * The Launcher class is the main game starter
      */
     public static void main(String[] args) {
+        // Enable opengl acceleration
         System.setProperty("sun.java2d.opengl", "true");
+
+        // Initilize Singleton
         Application.getInstance().init();
-        Game game = new Game();
+
         // Create and start game
         Game game = new Game(60);
         game.start();
